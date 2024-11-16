b6af652099da9872af7c025b20ffa3bfe95d4e32
JSK <jiashunkang2003@163.com>
2024-11-16 14:39:28 +0800

    CSMA v1

--- config.go
+++ config.go
@@ -12,6 +12,8 @@ const (
 	DATA_SIZE      = 500 //  500 data bit per frame
 	S_WINDOW_SIZE  = 2   // sliding window size: choose from 0-8
 	MAX_RESEND     = 8   // Max resend times before link error
+	RTT            = 200 // Round Trip Time in milisecond
+	POWER_SIGNAL   = 0.1 // >powersignal means signal, it is examnied by matlab
 )
 
 // PHY frame
