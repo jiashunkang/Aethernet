addd2df8c9757201a23e13e9ca70803ed6ac7494
JSK <jiashunkang2003@163.com>
2024-12-02 18:45:51 +0800

    edit parameter

--- config.go
+++ config.go
@@ -9,9 +9,9 @@ const (
 	PreambleLength = 48    // Length of preamble signal
 	FC             = 10000 // Carrier frequency
 	SYNC_PARA      = 0.2
-	DATA_SIZE      = 500 //  500 data bit per frame
-	S_WINDOW_SIZE  = 2   // sliding window size: choose from 0-8
-	MAX_RESEND     = 16  // Max resend times before link error
-	RTT            = 200 // Round Trip Time in milisecond
-	POWER_SIGNAL   = 0.1 // >powersignal means signal, it is examnied by matlab
+	DATA_SIZE      = 400  //  500 data bit per frame
+	S_WINDOW_SIZE  = 4    // sliding window size: choose from 0-8
+	MAX_RESEND     = 16   // Max resend times before link error
+	RTT            = 200  // Round Trip Time in milisecond
+	POWER_SIGNAL   = 0.04 // >powersignal means signal, it is examnied by matlab
 )
